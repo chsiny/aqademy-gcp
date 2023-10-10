4d183354bf5c58a4b2318129e76198948c17ce73
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2023-10-10 20:03:47 +1000

    Update upvote.js

--- src/assets/js/upvote.js
+++ src/assets/js/upvote.js
@@ -1,23 +1,49 @@
+// $(document).ready(function() {
+//     $('button#upvote-btn').click(function(e) {
+//       e.preventDefault(); // prevent default form submission
+//       var post_id = $(this).data('post-id'); // get the post ID from the button data attribute
+//       var btn = $(this); // store the button element in a variable
+//       $.ajax({
+//         type: 'POST',
+//         url: '../../upvote.php',
+//         data: { 'post_id': post_id },
+//         success: function(response) {
+//             console.log(response); // debug statement
+//             if (response.success) {
+//                 var upvotes = parseInt(response.upvotes);
+//                 $('small#upvote').text(upvotes);
+//                 btn.prop('disabled', true); // disable the button after upvoting
+//             }
+//           },
+//         error: function() {
+//           alert('Error upvoting post.'); // display an error message if the AJAX request fails
+//         }
+//       });
+//     });
+//   });
+
 $(document).ready(function() {
-    $('button#upvote-btn').click(function(e) {
+  $('button#upvote-btn').click(function(e) {
       e.preventDefault(); // prevent default form submission
       var post_id = $(this).data('post-id'); // get the post ID from the button data attribute
+      console.log('Post ID:', post_id); // Debug statement
       var btn = $(this); // store the button element in a variable
       $.ajax({
-        type: 'POST',
-        url: '../../upvote.php',
-        data: { 'post_id': post_id },
-        success: function(response) {
-            console.log(response); // debug statement
-            if (response.success) {
-                var upvotes = parseInt(response.upvotes);
-                $('small#upvote').text(upvotes);
-                btn.prop('disabled', true); // disable the button after upvoting
-            }
+          type: 'POST',
+          url: '../../upvote.php',
+          data: { 'post_id': post_id, 'csrf_token': csrfToken }, // replace 'csrfToken' with your actual token
+          dataType: 'json',
+          success: function(response) {
+              console.log('Response:', response); // Debug statement
+              if (response.status === 'success') {
+                  var upvotes = parseInt(response.upvotes);
+                  $('small#upvote').text(upvotes);
+                  btn.prop('disabled', true); // disable the button after upvoting
+              }
           },
-        error: function() {
-          alert('Error upvoting post.'); // display an error message if the AJAX request fails
-        }
+          error: function() {
+              alert('Error upvoting post.'); // display an error message if the AJAX request fails
+          }
       });
-    });
   });
+});

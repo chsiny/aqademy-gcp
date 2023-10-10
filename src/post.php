01d928258c42c058f3a12b0964ad328c2f3b1eb7
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2023-10-10 14:10:17 +1000

    Update post.php

--- src/post.php
+++ src/post.php
@@ -126,48 +126,50 @@ if (isset($_GET['id'])) {
         // Fetch the post data
         $post = $result->fetch_assoc();
     ?>
-    <h1><?php echo $post->title; ?></h1>
-    <p><?php echo $post->content; ?></p>
-    <p>Posted by <?php echo $post->username; ?> on <?php echo date('F j, Y, g:i a', strtotime($post->datetime)); ?></p>
-    <label>Likes: </label>
-    <small id="upvote"><?= $post->upvotes ?></small>
-    <br>
-    <button id="upvote-btn" class="btn btn-primary" data-post-id="<?= $post->postId ?>" data-url="#">Upvote</button>
-    <hr>
-    <h3>Comments</h3>
-    <?php if ($comments) : ?>
-        <?php foreach ($comments as $comment) : ?>
-        <div class="card mb-3">
-            <div class="card-body">
-            <h5 class="card-title"><?php echo $comment['username']; ?></h5>
-            <p class="card-text"><?php echo $comment['content']; ?></p>
-            <p class="card-text"><small class="text-muted">Posted on <?php echo date('F j, Y, g:i a', strtotime($comment['datetime'])); ?></small></p>
+        <h1><?php echo $post->title; ?></h1>
+        <p><?php echo $post->content; ?></p>
+        <p>Posted by <?php echo $post->username; ?> on <?php echo date('F j, Y, g:i a', strtotime($post->datetime)); ?></p>
+        <label>Likes: </label>
+        <small id="upvote"><?= $post->upvotes ?></small>
+        <br>
+        <button id="upvote-btn" class="btn btn-primary" data-post-id="<?= $post->postId ?>" data-url="#">Upvote</button>
+        <hr>
+        <h3>Comments</h3>
+        <?php if ($comments) { ?>
+            <?php foreach ($comments as $comment) : ?>
+            <div class="card mb-3">
+                <div class="card-body">
+                <h5 class="card-title"><?php echo $comment['username']; ?></h5>
+                <p class="card-text"><?php echo $comment['content']; ?></p>
+                <p class="card-text"><small class="text-muted">Posted on <?php echo date('F j, Y, g:i a', strtotime($comment['datetime'])); ?></small></p>
+                </div>
             </div>
-        </div>
-        <?php endforeach; ?>
-    <?php else : ?>
-        <p>No comments yet.</p>
-    <?php endif; ?>
-    <hr>
-    <h4>Add a comment</h4>
-    <form method="post" action="#">
-        <div class="form-group">
-        <input type="hidden" name="postId" value="<?php echo $post->postId; ?>">
-        <label for="content">Comment</label>
-        <textarea class="form-control" name="content" rows="3" required></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+            <?php endforeach; ?>
+        <?php } else { ?>
+            <p>No comments yet.</p>
+        <?php } endif; ?>
+        <hr>
+        <h4>Add a comment</h4>
+        <form method="post" action="#">
+            <div class="form-group">
+            <input type="hidden" name="postId" value="<?php echo $post->postId; ?>">
+            <label for="content">Comment</label>
+            <textarea class="form-control" name="content" rows="3" required></textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
 
-    <?php else : ?>
+    <?php } else { ?>
     <p>Post not found.</p>
-    <?php endif;
+    <?php } endif; 
     // Close the database connection
-    $conn->close();
+    $conn->close(); 
+
 } else {
     // Handle cases where no post ID is provided (e.g., show an error message)
     echo "Post not found.";
 }
+endif;
 ?>
 </div>
 

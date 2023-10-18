c36751f50c7ea65d0b4ef3828ed1d24dcee70d1f
Hsing-Yu Chen <34529973+chsiny@users.noreply.github.com>
2023-10-18 18:48:38 +1000

    Update init.sql

--- src/init.sql
+++ src/init.sql
@@ -7,21 +7,18 @@ CREATE TABLE users (
 CREATE TABLE posts (
     postId INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) FOREIGN KEY REFERENCES users(username),
     content TEXT NOT NULL,
     datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    upvotes INT DEFAULT 0,
-    FOREIGN KEY (username) REFERENCES users(username)
+    upvotes INT DEFAULT 0
 );
 
 CREATE TABLE comments (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
+    username VARCHAR(255) FOREIGN KEY REFERENCES users(username),
     content TEXT NOT NULL,
     datetime DATETIME DEFAULT CURRENT_TIMESTAMP,
-    postId INT NOT NULL,
-    FOREIGN KEY (username) REFERENCES users(username),
-    FOREIGN KEY (potId) REFERENCES posts(postId)
+    postId INT FOREIGN KEY REFERENCES posts(postId)
 );
 
 INSERT INTO users (username, password) VALUES ('admin', 'admin');
